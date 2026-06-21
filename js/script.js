60936695f806e63615cd42e185f6c87b405491b4
Franco Genoff <francogenoff@gmail.com>
2026-06-21 19:03:34 +0000

    Agregando funcion para imprimir los libros en el HTML

--- js/script.js
+++ js/script.js
@@ -1,25 +1,25 @@
 const miBiblioteca = [];
 
-function Libro(id, titulo, autor, descripcion) {
+function Libro(titulo, autor, descripcion, portada) {
     this.id = crypto.randomUUID();
     this.titulo = titulo;
     this.autor = autor;
     this.descripcion = descripcion;
+    this.portada = portada;
 }
 function añadirLibro(nuevoLibro) {
     miBiblioteca.push(nuevoLibro);
 }
 
+const meditaciones = new Libro("Meditaciones", "Marco Aurelio", "Conjunto de meditaciones filosoficas breves sobre la vida.", "https://m.media-amazon.com/images/I/71iRHcSUhtL._UF1000,1000_QL80_.jpg");
 
-const meditaciones = new Libro("Meditaciones", "Marco Aurelio", "Conjunto de meditaciones filosoficas breves sobre la vida.");
+const antifragil = new Libro("Antifragil", "Nassim Taleb", "Habla sobre un nuevo termino empleado por el autor, la antifragilidad, y como la vida y todo en ella se relaciona con lo fragil o antifragil.", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ0NZpHkX8ozzMA1og4qY1joAZkXnLNKVFzuMAA6SsY4vWKRAWKWwYrS7w&s=");
 
-const antifragil = new Libro("Antifragil", "Nassim Taleb", "Habla sobre un nuevo termino empleado por el autor, la antifragilidad, y como la vida y todo en ella se relaciona con lo fragil o antifragil.");
+const confesiones = new Libro("Confesiones", "San Agustin de Hipona", "Narra sobre la vida y reflexiones de San Agustin de Hipona, su experiencia con la religion y la vida", "https://image.cdn1.buscalibre.com/5b56caa14463b583508b4567.RS500x500.jpg");
 
-const confesiones = new Libro("Confesiones", "San Agustin de Hipona", "Narra sobre la vida y reflexiones de San Agustin de Hipona, su experiencia con la religion y la vida");
+const libroCincoAnillos = new Libro("El Libro de los Cinco Anillos", "Miyamoto Musashi", "Divide el libro en cinco tomos distintos donde cada uno narra el estilo de vida que se debe seguir para ser un guerrero completo", "https://http2.mlstatic.com/D_NQ_NP_909724-MLA99897222923_112025-O.webp");
 
-const libroCincoAnillos = new Libro("El Libro de los Cinco Anillos", "Miyamoto Musashi", "Divide el libro en cinco tomos distintos donde cada uno narra el estilo de vida que se debe seguir para ser un guerrero completo");
-
-const habitosAtomicos = new Libro("Habitos Atomicos", "James Clear", "Narra diversas estrategias o consejos para desarrollar buenos habitos");
+const habitosAtomicos = new Libro("Habitos Atomicos", "James Clear", "Narra diversas estrategias o consejos para desarrollar buenos habitos", "https://images.cdn1.buscalibre.com/fit-in/360x360/50/37/5037ad069dd27a4f1859acd5acac0fa3.jpg");
 
 añadirLibro(meditaciones);
 añadirLibro(antifragil);
@@ -27,4 +27,33 @@ añadirLibro(confesiones);
 añadirLibro(libroCincoAnillos);
 añadirLibro(habitosAtomicos);
 
-console.log(miBiblioteca);
+const contenedorLibros = document.querySelector(".contenedorLibros");
+function mostrarCatalogo() {
+    contenedorLibros.innerHTML = "";
+
+    miBiblioteca.forEach(libro => {
+
+    const tarjeta = document.createElement("div");
+    tarjeta.classList.add("tarjeta-libro");
+    
+    const titulo = document.createElement("h3");
+    titulo.classList.add("titulo-libro");
+    titulo.textContent = libro.titulo;
+
+    const autor = document.createElement("p");
+    autor.classList.add("autor-libro");
+    autor.textContent = `Autor: ${libro.autor}`;
+
+    const portada = document.createElement("img");
+    portada.classList.add("portada-libro");
+    portada.src = libro.portada;
+
+    tarjeta.appendChild(titulo);
+    tarjeta.appendChild(autor);
+    tarjeta.appendChild(portada);
+    contenedorLibros.appendChild(tarjeta);
+    });
+}
+mostrarCatalogo();
+
+
